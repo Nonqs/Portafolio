2b03c08c7b63cb665b9217eb041bb989f933383e
Nonqs <tomastru114@gmail,com>
2024-06-18 15:02:47 +0200

    Feature: Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import About from './components/pages/About';
 import Stack from './components/pages/MyStack';
 import { useIntersectionObserver } from './hook/IntersectionObserver';
 import Projects from './components/pages/Projects';
+import ContactPage from './components/pages/ContactPage';
 
 function App() {
 
   const [isSticky, setIsSticky] = useState(false)
   const aboutRef = useRef(null);
-  const isAboutVisible = useIntersectionObserver(aboutRef);
+  const isVisible = useIntersectionObserver(aboutRef);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,23 +37,32 @@ function App() {
       <section className="flex flex-col h-[100vh] items-center justify-between">
         <PresentationText />
         <Navbar isSticky={isSticky} />
-        <article className='mb-5 mt-40 animate-bounce'>
+        <article className='mb-5 mt-52 animate-bounce'>
           <button className="Btn rotate-180">
             <svg height="1.2em" className="arrow" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path></svg>
           </button>
         </article>
       </section>
 
-      <section className='h-[90vh] w-full flex justify-center items-center bg-gray-200' style={{ borderTop: "#01c38d 8px solid", borderBottom: "#01c38d 8px solid" }}>
+      <section ref={aboutRef} className='h-[90vh] w-full flex flex-col justify-center items-center bg-gray-200' style={{ borderTop: "#01c38d 8px solid", borderBottom: "#01c38d 8px solid" }}>
+        <div className='container'>
+          <h2 className={`text-[#01c38d] text-center mb-10 text-4xl font-bold p-2 border-animated w-full ${isVisible ? 'animate-border' : ''}`}>About Me</h2>
+        </div>
         <div className="flex w-full justify-center">
           <About />
           <Stack />
         </div>
       </section>
 
-      <section className='h-[100vh]'>
+      <section className='h-auto'>
         <div className='w-full flex justify-center mt-10'>
           <Projects />
+        </div>
+      </section>
+
+      <section className='h-[100vh]  bg-gray-200 ' style={{ borderTop: "#01c38d 8px solid", borderBottom: "#01c38d 8px solid" }}>
+        <div className='w-full flex justify-center mt-10'>
+          <ContactPage />
         </div>
       </section>
     </>
---
+++ src/components/pages/ContactPage.tsx
@@ -0,0 +1,126 @@
+import { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
+import { useIntersectionObserver } from '../../hook/IntersectionObserver';
+import email from "../../public/email.svg"
+import telephone from "../../public/telephone.svg"
+import github from "../../public/github.svg"
+
+export default function ContactPage() {
+
+  const contactRef = useRef<HTMLDivElement>(null)
+  const isVisible = useIntersectionObserver(contactRef)
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  })
+
+  const [status, setStatus] = useState('')
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const serviceID = process.env.SERVICE_ID
+    const templateID = process.env.TEMPLATE_ID
+    const userID = process.env.USER_ID
+
+    if (serviceID && templateID && userID) {
+
+      emailjs.send(serviceID, templateID, formData, userID)
+        .then((response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          setStatus('Message sent successfully!');
+        }, (err) => {
+          console.error('FAILED...', err);
+          setStatus('Failed to send message. Please try again.');
+        })
+
+    }
+
+  }
+
+  return (
+    <div ref={contactRef} className="container flex flex-col items-center justify-center">
+      <h2 className={`text-[#01c38d] text-center mb-10 text-4xl font-bold p-2 border-animated w-full ${isVisible ? 'animate-border' : ''}`}>Contact Me</h2>
+      <div className='flex w-full justify-center'>
+        <form onSubmit={handleSubmit} className="bg-white p-6 rounded-xl shadow-lg w-1/2 max-w-lg">
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-black font-semibold mb-2">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-black font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-black font-semibold mb-2">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md h-32"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-[#01c38d] text-white p-2 rounded-md hover:bg-[#00a377] transition duration-300"
+            >
+              Send
+            </button>
+          </div>
+          {status && <p className="text-center mt-4">{status}</p>}
+        </form>
+        <div className='w-1/2 h-full flex items-center justify-center'>
+          <div className='flex flex-col justify-evenly h-full'>
+            <article className='flex items-center'>
+              <img src={email} alt="" className='p-2 w-16 bg-white rounded-xl shadow-lg max-w-lg me-5 hover:drop-shadow-2xl transform hover:scale-110 transition-transform duration-200 ease-in-out' />
+              <div className='flex flex-col'>
+                <span className='font-semibold'>Email</span>
+                <span className='font-thin'>tomastrujillogz@gmail.com</span>
+              </div>
+            </article>
+            <article className='flex items-center'>
+              <img src={telephone} alt="" className='p-2 w-16 bg-white rounded-xl shadow-lg max-w-lg me-5 hover:drop-shadow-2xl transform hover:scale-110 transition-transform duration-200 ease-in-out' />
+              <div className='flex flex-col'>
+                <span className='font-semibold'>Telephone</span>
+                <span className='font-thin'>No habemus</span>
+              </div>
+            </article>
+            <article className='flex items-center'>
+              <img src={github} alt="" className='p-2 w-16 bg-white rounded-xl shadow-lg max-w-lg me-5 hover:drop-shadow-2xl transform hover:scale-110 transition-transform duration-200 ease-in-out' />
+              <div className='flex flex-col'>
+                <span className='font-semibold'>Github</span>
+                <span className='font-thin'>@Nonqs</span>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
