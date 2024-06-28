2fdc8b6db44e3874d4309c37e714ad08c8b5be81
Tomas Trujillo <tomastru114@gmail.com>
2024-06-28 11:51:26 +0200

    Feature: Improved performance

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -3,35 +3,64 @@ import { useIntersectionObserver } from "../../hook/IntersectionObserver";
 import ProjectAnimation from "../utils/ProjectAnimation";
 import { Project } from "../../types/types";
 
-import insomnia from "../../public/insomnia.svg"
-import sql from "../../public/sql.svg"
-import java from "../../public/java.svg"
+import css from "../../public/svgs/css.svg"
+import mysql from "../../public/svgs/mysql.svg"
+import prisma from "../../public/svgs/prisma.svg"
+import ts from "../../public/svgs/ts.svg"
+import tailwind from "../../public/svgs/tailwind.svg"
+import next from "../../public/svgs/next.svg"
+import react from "../../public/svgs/react.svg"
+import vite from "../../public/svgs/vite-svgrepo-com.svg"
+
+import fin1 from "../../public/imgs/fin-1.webp"
+import fin2 from "../../public/imgs/fin-2.webp"
+import fin3 from "../../public/imgs/fin-3.webp"
+import fin4 from "../../public/imgs/fin-4.webp"
+import math1 from "../../public/imgs/math-1.webp"
+import math2 from "../../public/imgs/math-2.webp"
+import math3 from "../../public/imgs/math-3.webp"
+import subs1 from "../../public/imgs/subs1.webp"
+import subs2 from "../../public/imgs/subs2.webp"
+import subs3 from "../../public/imgs/subs3.webp"
+import subs4 from "../../public/imgs/subs4.webp"
 
 export default function Projects() {
-    
+
     const projectsRef = useRef<HTMLDivElement>(null);
     const isVisible = useIntersectionObserver(projectsRef);
 
     const projects: Project[] = [
         {
-            name: "Financial Tracker",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, est illum magni quisquam perferendis quam. Error dolore ullam molestias beatae. Facilis eaque dolorem sapiente qui recusandae. Ducimus numquam expedita dolore?",
-            technologies: [{svg: insomnia, alt:"insomnia" }, {svg: sql, alt: "sql"}, {svg: java, alt:"java"}]
+            name: "FinTrack",
+            github: "https://github.com/Nonqs/financial",
+            description: "Fintrack is a financial tracker where you can effortlessly track all your income and expenses. It allows you to categorize each transaction, allocate them to designated accounts, and categorize your income and expenses depending on where they come from",
+            technologies: [{ svg: ts, alt: "ts logo" }, { svg: react, alt: "react logo" }, { svg: next, alt: "next logo" }, { svg: mysql, alt: "mysql logo" }, { svg: tailwind, alt: "tailwind logo" }, { svg: prisma, alt: "prisma logo" }],
+            images: [{svg:fin1, alt:"img1" },{svg:fin2, alt:"img1" },{svg:fin3, alt:"img1" },{svg:fin4, alt:"img1" }]
         },
-        {
-            name: "Math",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, est illum magni quisquam perferendis quam. Error dolore ullam molestias beatae. Facilis eaque dolorem sapiente qui recusandae. Ducimus numquam expedita dolore?",
-            technologies: [{svg: insomnia, alt:"insomnia" }, {svg: sql, alt: "sql"}, {svg: java, alt:"java"}]
+        { 
+            name: "Substancial",  
+            description: "Landing page for Substancial restaurant, in which I worked as co-designer and programmer.<br></br>(In development) ",
+            technologies: [ {svg:vite, alt:"vite logo"}, { svg: ts, alt: "ts logo" }, { svg: react, alt: "sql" }, { svg: css, alt: "java" }, {svg: tailwind, alt:"tailwind logo"}],
+            images: [{svg:subs2, alt:"img1" },{svg:subs1, alt:"img1" },{svg:subs3, alt:"img1" }, {svg:subs4, alt:"img1" }]
+        },
+        { 
+            name: "Math.simple",  
+            web: "https://mathsimple.netlify.app",
+            github: "https://github.com/Nonqs/Math.simple",
+            description: "Math.simple is a math problem generator. It was planned to help people who want to practice exercise and don't want to search through a lot of worksheets that have no answers or ways to validate their results.<br /><br />The project is in demo; it only generates subtraction, additions, multiplications and division, but my plan is to add new features, from powers and roots to matrixes.",
+            technologies: [{svg:vite, alt:"vite logo"}, { svg: ts, alt: "ts logo" }, { svg: react, alt: "sql" }, { svg: css, alt: "java" }],
+            images: [{svg:math1, alt:"img1" },{svg:math2, alt:"img1" },{svg:math3, alt:"img1" }]
         }
-    ]
+    ];
+
 
     return (
         <div ref={projectsRef} className="container flex flex-col items-center justify-center">
             <h4 className={`text-[#01c38d] text-center mb-10 text-4xl font-bold p-2 border-animated w-full ${isVisible ? 'animate-border' : ''}`}>
                 Projects
             </h4>
-            <div className="h-[40vh] w-full mt-10">
-                <ProjectAnimation projects={projects}/>
+            <div className="h-auto w-full mt-10">
+                <ProjectAnimation projects={projects} />
             </div>
         </div>
     );
--- src/components/utils/ProjectAnimation.tsx
+++ src/components/utils/ProjectAnimation.tsx
@@ -7,11 +7,13 @@ import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import "../../styles/Projects.css"
-import github from "../../public/github.svg"
+import github from "../../public/svgs/github.svg"
+import web from "../../public/svgs/web.svg"
 import { Project } from '../../types/types';
 import { useIntersectionObserver } from '../../hook/IntersectionObserver';
 import { useRef } from 'react';
 import TechBaul from './TechBaul';
+import Background from './Background';
 
 export default function ProjectAnimation({ projects }: { projects: Project[] }) {
 
@@ -21,10 +23,24 @@ export default function ProjectAnimation({ projects }: { projects: Project[] }) 
    return (
       <section ref={projectRef}>
          {projects.map((project, index) => (
-            <div key={index} className={`flex justify-center mb-20 h-[80h] bg-gray-200 rounded-xl drop-shadow-2xl backdrop-blur-sm  ${isVisible ? ("animate-fade-in") : ("opacity-0")}`} style={{ borderTop: "#01c38d 8px solid", borderBottom: "#01c38d 8px solid" }}>     
-               <a className='absolute top-1 left-1 p-1 rounded-xl border border-black shadow-inner' href="">
-                  <img className='w-10 opacity-100' src={github} alt="" />
-               </a>
+            <div key={index} className={`flex justify-center mb-20 h-[80h] rounded-xl drop-shadow-2xl backdrop-blur-sm  ${isVisible ? ("animate-fade-in") : ("opacity-0")}`} style={{ borderTop: "#01c38d 8px solid" }}>
+               <Background />
+               {project.github &&
+                  (
+                     <a className='absolute top-1 left-1 z-50' href={project.github} target="_blank" rel="noopener noreferrer">
+                        <div className={`p-1 bg-white border border-black drop-shadow-lg me-2 mb-2 rounded-xl hover:drop-shadow-2xl transform hover:scale-110 transition-transform duration-200 ease-in-out`}>
+                           <img className='w-10' src={github} alt="" />
+                        </div>
+                     </a>
+                  )}
+               {project.web &&
+                  (
+                     <a className='absolute top-1 left-16 z-50' href={project.web} target="_blank" rel="noopener noreferrer">
+                        <div className={`p-1 bg-white border border-black drop-shadow-lg me-2 mb-2 rounded-xl hover:drop-shadow-2xl transform hover:scale-110 transition-transform duration-200 ease-in-out`}>
+                           <img className='w-10' src={web} alt="" />
+                        </div>
+                     </a>
+                  )}
                <div className='flex w-4/7'>
                   <div className="animationContainer">
                      <Swiper
@@ -48,9 +64,9 @@ export default function ProjectAnimation({ projects }: { projects: Project[] }) 
                         modules={[EffectCoverflow, Pagination, Navigation]}
                         className="swiper_container"
                      >
-                        {project.technologies.map(tech => (
+                        {project.images.map(tech => (
                            <SwiperSlide className='flex justify-center' key={tech.alt}>
-                              <img src={tech.svg} alt={tech.alt} />
+                              <img className='w-[40rem] shadow-xl' src={tech.svg} alt={tech.alt}/>
                            </SwiperSlide>
                         ))}
                         <div className="slider-controler">
@@ -59,20 +75,31 @@ export default function ProjectAnimation({ projects }: { projects: Project[] }) 
                      </Swiper>
                   </div>
                </div>
-               <div className='w-3/7 h-full flex flex-col mt-10 me-20'>
-                  <article className='flex flex-col mb-10'>
+               <div className='w-3/7 h-full flex flex-col mt-10 ms-10 me-10'>
+                  <article className='flex flex-col mb-5'>
                      <h4 className={`text-[#01c38d] mb-2 text-4xl font-semibold p-2 border-animated-right ${isVisible ? 'animate-border-right' : ''}`}>
                         {project.name}
                      </h4>
-                     <span>{project.description}</span>
+                     <span dangerouslySetInnerHTML={{ __html: project.description }}></span>
                   </article>
-                  <div className="rounded-xl shadow-inner p-1 flex justify-center bottom-0" style={{ border: "white 1px solid" }}>
+                  <div className="rounded-xl shadow-inner p-1 flex justify-center bottom-0 border border-gray-200 bg-white">
                      <TechBaul isVisible={isVisible} svgs={project.technologies} />
                   </div>
                </div>
 
             </div>
          ))}
+         <article className='w-full flex flex-col justify-center items-center mb-20'>
+            <h4 className={`text-[#01c38d] text-center text-xl p-2 font-thin`}>
+               See more on my github
+            </h4>
+            <a href="https://github.com/Nonqs" target="_blank" rel="noopener noreferrer">
+               <div className={`p-1 bg-white border border-black flex justify-center items-center w-28 drop-shadow-lg me-2 mb-2 rounded-xl hover:drop-shadow-2xl transform hover:scale-110 transition-transform duration-200 ease-in-out`}>
+                  <img className='w-10' src={github} alt="" />
+                  <span>GitHub</span>
+               </div>
+            </a>
+         </article>
       </section >
    );
 }
